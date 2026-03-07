8195ff7ca7e23870db9981bca6cba5efe492aba9
Arihant Marwaha <arihantmarwaha@icloud.com>
2026-03-07 14:53:22 +0530

    Initial Commit

---
+++ TimesOut/TimesOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesOutApp.swift
+//  TimesOut
+//
+//  Created by Arihant Marwaha on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TimesOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
